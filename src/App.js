c7dee031de691ff4a09ab32dd5419c63c6737dd3
Dhananjay Tanpure <dtan13@pm.me>
2020-10-22 09:28:19 +0530

    Some changes for trial deploy

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Website is under construction.
+        </h3>
       </header>
     </div>
   );
