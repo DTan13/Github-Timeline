37c6a5eb840f1aa67bfe850adcc9b31d079f8a6e
Dhananjay Tanpure <dtan13@pm.me>
2020-10-22 21:55:21 +0530

    added SocialMedia component to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import SocialMedia from './components/socialMedia';
+
 import './App.css';
+import './styles/social.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Website is under construction.
-        </h3>
+        <h2>
+          Coming Soon!
+        </h2>
+        <SocialMedia />
       </header>
     </div>
   );
