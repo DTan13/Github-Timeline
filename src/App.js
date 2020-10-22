7bcfa482584608c496f55bd9ce893ac6aba9e9a5
Dhananjay Tanpure <dtan13@pm.me>
2020-10-23 00:46:29 +0530

    Minor fixes and added project Link

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
         <h2>
           Coming Soon!
         </h2>
+        <br />
+        <a href="https://github.com/dtan13/dtan13.github.io">Check Progress</a>
         <SocialMedia />
         <img src="https://visitor-badge.glitch.me/badge?page_id=dtan13.dtan13.github.io" alt="Visitor Counter" />
       </header>
