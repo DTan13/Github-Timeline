98434cc46a0119a0dd52d997acb1421dc0063bc4
Dhananjay Tanpure <dtan13@pm.me>
2020-10-22 21:50:58 +0530

    added links for social media

---
+++ src/data/socialMediaLinks.js
@@ -0,0 +1,11 @@
+export const socialMediaLinks = [
+    { Instagram: "https://www.instagram.com/_dtan13_/" },
+    { Telegram: "https://telegram.me/dtan13" },
+    { Gitlab: "https://gitlab.com/_dtan13_/" },
+    { Github: "https://github.com/dtan13" },
+    { Reddit: "https://reddit.com/user/DTan13" },
+    { Stackoverflow: "https://stackoverflow.com/users/10990171/dtan13" },
+    { Discord: "https://discord.gg/befXQxu" },
+    { Linkedin: "https://linkedin.com/in/dtan13/" },
+    { Twitter: "https://twitter.com/_DTan13_" }
+]
