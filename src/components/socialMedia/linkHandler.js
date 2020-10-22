acfa34c0b9bb67b60378082a58bc8170546b5fde
Dhananjay Tanpure <dtan13@pm.me>
2020-10-22 21:53:02 +0530

    created LinkHandler to handle social media links

---
+++ src/components/socialMedia/linkHandler.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaDiscord, FaGithub, FaGitlab, FaInstagram, FaLinkedin, FaRedditAlien, FaStackOverflow, FaTelegram, FaTwitter, } from "react-icons/fa";
+
+const LinkHandler = props => {
+    const getComponent = type => {
+        console.log(type);
+        switch (type) {
+            case "Instagram":
+                return <FaInstagram size='50px' />
+            case "Telegram":
+                return <FaTelegram size='50px' />
+            case "Gitlab":
+                return <FaGitlab size='50px' />
+            case "Github":
+                return <FaGithub size='50px' />
+            case "Reddit":
+                return <FaRedditAlien size='50px' />
+            case "Stackoverflow":
+                return <FaStackOverflow size='50px' />
+            case "Discord":
+                return <FaDiscord size='50px' />
+            case "Linkedin":
+                return <FaLinkedin size='50px' />
+            case "Twitter":
+                return <FaTwitter size='50px' />
+            default:
+                return;
+        }
+    }
+
+    return (
+        <div className="handler-wrapper">
+            <a href={props.handlerLink}>
+                {getComponent(props.type)}
+            </a>
+        </div>
+    )
+}
+
+export default LinkHandler;
