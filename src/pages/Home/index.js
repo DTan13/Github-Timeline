148657b6d5c1b292eddb11cec47e5c92e0e83c5e
Dhananjay Tanpure <dtan13@pm.me>
2020-10-24 20:26:53 +0530

    Temporary Home Component

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const Home = props => {
+    return (
+        <Layout>
+            <div className="home-page-wrapper">
+                Home Page Works
+            </div>
+        </Layout>
+    );
+};
+
+export default Home;
