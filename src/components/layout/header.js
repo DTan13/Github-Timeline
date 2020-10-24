e29e179e7e43434a1bf74d877d0bd41531a2ab06
Dhananjay Tanpure <dtan13@pm.me>
2020-10-24 20:25:51 +0530

    Added icons to layout links

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaHome, FaClock, FaCode, FaInfo } from "react-icons/fa";
 import { siteData } from '../../data/siteData';
 
 const Header = props => {
+    const getIcon = (title) => {
+        switch (title) {
+            case '/':
+                return <FaHome />;
+            case '/timeline':
+                return <FaClock />;
+            case '/projects':
+                return <FaCode />;
+            case '/about':
+                return <FaInfo />;
+            default:
+                break;
+        }
+    };
+
     return (
         <div className="header-wrapper">
             <div className="header-name">
@@ -19,6 +35,7 @@ const Header = props => {
                             key={`page${index}`}
                             title={page.title}
                         >
+                            {getIcon(page.link.toLowerCase())}
                             <span className="header-page-single-text">
                                 {` ${page.title}`}
                             </span>
