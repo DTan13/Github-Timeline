0e1141586cc12ac47c4092f5639911ee2ef1d40e
Dhananjay Tanpure <dtan13@pm.me>
2020-10-24 23:37:03 +0530

    removed Stackoverflow and Discord from links

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -3,8 +3,6 @@ export const socialMediaLinks = [
     { Telegram: "https://telegram.me/dtan13" },
     { Gitlab: "https://gitlab.com/_dtan13_/" },
     { Github: "https://github.com/dtan13" },
-    { Stackoverflow: "https://stackoverflow.com/users/10990171/dtan13" },
-    { Discord: "https://discord.gg/befXQxu" },
     { Linkedin: "https://linkedin.com/in/dtan13/" },
     { Twitter: "https://twitter.com/_DTan13_" }
 ];
