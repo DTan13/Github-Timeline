7e23607ffba8b74b4b550abff28f547c87d82252
Dhananjay Tanpure <dtan13@pm.me>
2020-10-25 13:49:24 +0530

    created Event and get data from githubAPI

---
+++ src/pages/Home/Event.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Event = props => {
+    return (
+        <div className="event-wrapper">
+        </div>
+    );
+};
+
+export default Event;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../components/layout';
+import Axios from 'axios';
+import Event from './Event';
 
+import { siteData } from '../../data/siteData';
 const Home = props => {
+    const [events, setEvents] = useState([]);
+    const [page, setPage] = useState(1);
+    useEffect(() => {
+        Axios.get(`https://api.github.com/users/${siteData.githubUsername}/events?per_page=10&page=${page}`).then(res => {
+            setEvents([...events, ...res.data]);
+        }).catch(err => {
+            console.log(err);
+        });
+    }, [page]);
+
     return (
         <Layout>
             <div className="home-page-wrapper">
-                Home Page Works
+                <div className="event-list-wrapper">
+                    {events.map(event => {
+                        return (
+                            <div key={event.id}>
+                                <Event event={event} />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </Layout>
     );
