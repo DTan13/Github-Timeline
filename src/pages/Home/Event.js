bad1855e2d69e73a3d93e104787a25ad9ea096bc
Dhananjay Tanpure <dtan13@pm.me>
2020-10-25 13:52:28 +0530

    added discription for github Events

--- src/pages/Home/Event.js
+++ src/pages/Home/Event.js
@@ -55,10 +55,54 @@ const Event = props => {
                 break;
         }
     };
+    const getEventDesc = gitEvent => {
+        switch (gitEvent) {
+            case eventList.create:
+                return "Created Repository";
+            case eventList.issueComment:
+            case eventList.reviewComment:
+            case eventList.commitComment:
+                return "Added a comment";
+            case eventList.delete:
+                return "Deleted Repository";
+            case eventList.fork:
+                return "Forked Repository";
+            case eventList.issues:
+                return "Opened a Issue";
+            case eventList.pullreq:
+                return "Created a Pull Request";
+            case eventList.push:
+                return "Pushed to Repository";
+            case eventList.member:
+                return "Added member to Repository";
+            case eventList.wiki:
+                return "Added to Wiki";
+            case eventList.release:
+                return "Released new version";
+            case eventList.watch:
+                return "Started Watching";
+            case eventList.prReview:
+                return "Requested Review";
+            default:
+                break;
+        }
+    };
+
     return (
         <div className="event-wrapper">
             <div className="event-icon">
                 {getEventIcon(props.event.type)}
+            </div>
+            <div className="event-content">
+                <div className="event-content-title">
+                    {getEventDesc(props.event.type)}
+                </div>
+                <div className="event-content-repo">
+                    {props.event.repo.name}
+                </div>
+                <div className="event-content-time">
+                    {(new Date(Date.parse(props.event.created_at))).toUTCString()}
+                </div>
             </div>
         </div>
     );
