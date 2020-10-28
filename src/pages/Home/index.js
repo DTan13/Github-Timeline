3365bdc2adad62ce110c983cda5ca5ef036783e5
Dhananjay Tanpure <dtan13@pm.me>
2020-10-28 06:51:20 +0530

    display EventDetails

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,17 @@ import Axios from 'axios';
 import Event from './Event';
 import ReactVisibilitySensor from "react-visibility-sensor";
 
+import Layout from '../../components/layout';
 import { siteData } from '../../data/siteData';
 import { FaSpinner } from 'react-icons/fa';
+import EventDetails from './EventDetails';
 
 const Home = props => {
     const [events, setEvents] = useState([]);
     const [page, setPage] = useState(1);
     const [dataOver, setDataOver] = useState(false);
+    const [selectedId, setSelectedID] = useState(null);
+    const [showDetails, setShowDetails] = useState(false);
 
     useEffect(() => {
         Axios.get(`https://api.github.com/users/${siteData.githubUsername}/events?per_page=10&page=${page}`).then(res => {
@@ -37,6 +41,7 @@ const Home = props => {
                     <ReactVisibilitySensor onChange={() => { setPage(page + 1); }}>
                         <div className='event-load'>{dataOver ? 'Events are over' : <FaSpinner className='fa-spin' />}</div>
                     </ReactVisibilitySensor>
+                    <EventDetails hideDetails={() => { setShowDetails(false); }} id={selectedId} event={events.filter(event => event.id === selectedId)} />
                 </div>
             </div>
         </Layout>
