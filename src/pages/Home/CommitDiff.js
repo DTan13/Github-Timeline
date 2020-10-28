881b293b2101e3fd04a1c7e9335689608cc43ab1
Dhananjay Tanpure <dtan13@pm.me>
2020-10-28 06:46:57 +0530

    added CommitDiff for rendering diff

---
+++ src/pages/Home/CommitDiff.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { html } from "diff2html";
+
+
+const CommitDiff = props => {
+    // TODO Cleanup
+    // KNOWNISSUE :- diff2html --> Rendering big Files!!!!
+    useEffect(() => {
+        document.getElementById('commit-diff').innerHTML = html(props.diff, { drawFileList: true, matching: 'lines', diffStyle: "char", outputFormat: 'line-by-line' });
+    });
+
+    return (
+        <div id='commit-diff'></div>
+    );
+};
+
+export default CommitDiff;
