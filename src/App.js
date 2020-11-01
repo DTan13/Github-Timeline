8baa621096a29d4a3271ec7b651c764acbd91d6d
Dhananjay Tanpure <dtan13@pm.me>
2020-11-01 17:16:23 +0530

    env file and Minor Changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Projects from './pages/Projects';
 import Layout from './components/layout';
 
 class App extends React.Component {
+
   state = {
     user: ''
   };
@@ -21,6 +22,7 @@ class App extends React.Component {
     }
     );
   };
+  // TODO Global Axios Configuration
 
   render() {
     return (
