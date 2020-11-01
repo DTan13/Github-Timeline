259255a7802ff2cf9dcfb56361b54493b0c37d0c
Dhananjay Tanpure <dtan13@pm.me>
2020-11-01 17:15:21 +0530

    Added Feedback Component

---
+++ src/components/feedback/Feedback.js
@@ -0,0 +1,65 @@
+import Axios from 'axios';
+import React, { useState } from 'react';
+
+const Feedback = props => {
+    const [feedBack, setFeedback] = useState('');
+    const [name, setName] = useState('');
+    const [mail, setMail] = useState('');
+
+    const submitFeedback = () => {
+        if (feedBack === '' || name === '' || mail === '') {
+            alert("Please provide Data");
+            return;
+        }
+
+        Axios.post(process.env.REACT_APP_FEEDBACK_URL,
+            {
+                "name": name,
+                "email": mail,
+                "feedback": feedBack
+            }).then(res => {
+                alert('Thank you for feedback');
+            }).catch(err => {
+
+            });
+    };
+
+    return (
+        <div className="feedback-wrapper">
+            <div className="feedback-component">
+                <div className="feedback">
+                    Feedback
+                    <br />
+                    <hr />
+                    <br />
+                    <textarea maxLength='30' value={feedBack} placeholder=" Enter Feedback ..." onChange={(event) => {
+                        setFeedback(event.target.value);
+                    }}>
+
+                    </textarea>
+                </div>
+                <br />
+                <div className="feedback-bottom">
+                    <div className="name">
+                        Name <br /><br />
+                        <input type="text" value={name} placeholder=" Your Name ..." onChange={(event) => {
+                            setName(event.target.value);
+                        }} />
+                    </div>
+                    <div className="contact">
+                        Contact <br />
+                        <br />
+                        <input type="text" value={mail} placeholder=" Your Email" onChange={(event) => {
+                            setMail(event.target.value);
+                        }} />
+                    </div>
+                </div>
+                <br />
+                <br />
+                <button onClick={submitFeedback}>Submit Feedback</button>
+            </div>
+        </div>
+    );
+};
+
+export default Feedback;;
--- src/pages/Data/index.js
+++ src/pages/Data/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Feedback from '../../components/feedback/Feedback';
 let user = '';
 const Data = props => {
+
     return (
         <div className="data-wrapper">
             <div className="input-wrapper">
                 <input type="text" required placeholder='Github Username ex:DTan13' value={props.user || (user = window.location.pathname.split('/').reverse()[0])} onChange={event => { user = event.target.value; props.setUser(event.target.value); }} />
                 <br />
-                <button>
-                    <NavLink exact onClick={(event) => {
-                    console.log(user);
-                    props.setUser(user);
-                    if (!user) {
-                        alert('Enter A Username!');
-                        event.preventDefault();
-                    }
-                    }} to={`/home`}>Submit</NavLink>
-                </button>
+                <div>
+                    <button>
+                        <NavLink exact onClick={(event) => {
+                            props.setUser(user);
+                            if (!user) {
+                                alert('Enter A Username!');
+                                event.preventDefault();
+                            }
+                        }} to={`/home`}>See TimeLine</NavLink>
+                    </button>
+                    <button>
+                        <NavLink exact onClick={(event) => {
+                            props.setUser(user);
+                            if (!user) {
+                                alert('Enter A Username!');
+                                event.preventDefault();
+                            }
+                        }} to={`/projects`}>See Projects</NavLink>
+                    </button>
+                </div>
             </div>
+            <Feedback />
         </div>
     );
 };
