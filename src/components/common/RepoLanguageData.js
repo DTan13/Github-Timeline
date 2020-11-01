f65c5eebba5a5264de280c11a6ebea1eca29e9d2
Dhananjay Tanpure <dtan13@pm.me>
2020-11-01 17:12:03 +0530

    Updated condition for showing LanguageStats

--- src/components/common/RepoLanguageData.js
+++ src/components/common/RepoLanguageData.js
@@ -6,17 +6,17 @@ const RepoLanguageData = props => {
     totalLines = (keys.map(key => props.languageData[key]));
     totalLines = totalLines.reduce((a, b) => a + b, 0);
     return (
-        totalLines ? (<div className="repo-lang-data">
+        (<div className="repo-lang-data">
             <h3>Langauge Statistics</h3>
             <hr />
-            <div className="repo-lang-statistics">
+            {totalLines ? <div className="repo-lang-statistics">
                 {keys.map(key => {
                     return (<div key={key}>
                         {`${key} : ${((props.languageData[key] * 100) / totalLines).toFixed(2)}%`}
                     </div>);
                 })}
-            </div>
-        </div >) : null
+            </div> : [<br />, <h4>Nothing to show here!</h4  >]}
+        </div >)
     );
 };
 
