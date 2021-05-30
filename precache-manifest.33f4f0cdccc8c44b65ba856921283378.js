eb07ecfa5f615dc8b0736c9ba34c96dffc5a6788
Dhananjay Tanpure <dtan13@pm.me>
2021-05-30 03:30:52 +0000

    Updates

---
+++ precache-manifest.33f4f0cdccc8c44b65ba856921283378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0842157b23d0598b6b077f04e2b7dc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "741161692cef34aff974",
+    "url": "/static/css/main.fadc578e.chunk.css"
+  },
+  {
+    "revision": "3c2783e343ae2e204afc",
+    "url": "/static/js/2.7850cdcd.chunk.js"
+  },
+  {
+    "revision": "40f782f137a0497a9463d0cdc50c53b6",
+    "url": "/static/js/2.7850cdcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741161692cef34aff974",
+    "url": "/static/js/main.4f73ba5b.chunk.js"
+  },
+  {
+    "revision": "611d0d10ae662c0ebe19",
+    "url": "/static/js/runtime-main.6dbf3da8.js"
+  },
+  {
+    "revision": "12ee0906aa398bc5c7465cd29212a704",
+    "url": "/static/media/logo.12ee0906.svg"
+  }
+]);
